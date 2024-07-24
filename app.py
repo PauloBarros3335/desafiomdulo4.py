0a98ebc1ca94a2a8caabdcbccec34ef2b8808b1c
Paulo Barros <barrosp335@gmail.com>
2024-07-24 11:21:48 -0300

    Quito   commit

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-# from flask import Flask # type: ignore
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-
-# if __name__ == '__main__':
-#     app.run()
 
 
 
-from flask import Flask
+
+from flask import Flask # type: ignore
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/ ')
 def index():
-    return 'Hello, World!'
+    return 'Meu projeto em Python Do desavio da CesarSchool em noções de Programação, está fucionando perfeitamente!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+    
